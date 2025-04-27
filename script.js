d3003f72148e4dea4b4a3627b53bee74a8ad30bc
314trueshka <lovemoma1337@gmail.com>
2025-04-27 18:17:12 +0300

    добавил скрол по горизонту

--- script.js
+++ script.js
@@ -45,3 +45,33 @@ document.addEventListener('DOMContentLoaded', function() {
         card.querySelector('p').style.minHeight = maxHeight + 'px';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollContainer = document.querySelector('.scroll-container');
+    
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    scrollContainer.addEventListener('mousedown', (e) => {
+        isDown = true;
+        startX = e.pageX - scrollContainer.offsetLeft;
+        scrollLeft = scrollContainer.scrollLeft;
+    });
+
+    scrollContainer.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+
+    scrollContainer.addEventListener('mouseup', () => {
+        isDown = false;
+    });
+
+    scrollContainer.addEventListener('mousemove', (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - scrollContainer.offsetLeft;
+        const walk = (x - startX) * 2;
+        scrollContainer.scrollLeft = scrollLeft - walk;
+    });
+});
